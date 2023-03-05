b9738e4e7281abb2a1ad3647d3d4f0b26a60c8e5
Tharun S kumar <75157473+Tsk29@users.noreply.github.com>
2023-03-05 22:05:49 +0530

    Add files via upload

---
+++ tictac/tt.js
@@ -0,0 +1,96 @@
+const squares = document.querySelectorAll('.square');
+let currentPlayer = 'X';
+
+squares.forEach(square => {
+    square.addEventListener('click', handleClick);
+});
+
+
+function handleClick(event) {
+    var xColor = "#00FFFF";
+    var oColor = "#FF10F0";
+    const square = event.target;
+
+    if (square.textContent !== '') {
+        return;
+    }
+
+    square.textContent = currentPlayer;
+
+
+
+    if (checkForWin()) {
+        setTimeout(() => {
+            showModal(`${currentPlayer} wins!`);
+
+        }, 100);
+        // resetGame();
+        return;
+    }
+
+    if (checkForTie()) {
+        showModal(`It's a tie!`);
+        resetGame();
+        return;
+    }
+
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    var color = currentPlayer === "X" ? xColor : oColor; // to  add color 
+    this.style.color = color;
+}
+
+function checkForWin() {
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8], // horizontal
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8], // vertical
+        [0, 4, 8],
+        [2, 4, 6] // diagonal
+    ];
+
+    return winningCombos.some(combo => {
+        return combo.every(index => {
+            return squares[index].textContent === currentPlayer;
+        });
+    });
+}
+
+function checkForTie() {
+    return Array.from(squares).every(square => {
+        return square.textContent !== '';
+    });
+}
+
+function resetGame() {
+    squares.forEach(square => {
+        square.textContent = '';
+    });
+    currentPlayer = 'X';
+}
+
+
+function changeBackground() {
+    var wallpapers = ["https://images.hdqwalls.com/wallpapers/cyberpunk-neon-with-sword-4k-8k.jpg", "https://images.hdqwalls.com/wallpapers/neon-light-guy-4k-ve.jpg", "https://i.pinimg.com/564x/31/3e/a5/313ea52e1725f7f0c2d028308453700a.jpg"]; // array of wallpaper image URLs
+    var randomWallpaper = wallpapers[Math.floor(Math.random() * 3)]; // select a random wallpaper
+
+    document.body.style.backgroundImage = "url('" + randomWallpaper + "')"; // set the background image of the body
+    document.body.style.backgroundSize = "cover"; // make sure the image covers the entire background
+}
+
+setInterval(changeBackground, 300);
+
+function showModal(message) {
+    const modal = document.querySelector('.modal');
+    const messageElement = document.querySelector('.modal-message');
+    const button = document.querySelector('.modal-button');
+
+    messageElement.textContent = message;
+    modal.style.display = 'flex';
+
+    button.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+}
