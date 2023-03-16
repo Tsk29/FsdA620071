649df59551ab14c25ed1669b5d59e25e21ee6f56
Tharun S kumar <75157473+Tsk29@users.noreply.github.com>
2023-03-16 15:44:39 +0530

    Update npa.js

--- Photo/npa.js
+++ Photo/npa.js
@@ -12,13 +12,52 @@ function makeBlueish() {
 }
 
 function makeBw() {
-    const img = document.getElementById('i9');
+    var img = document.getElementById("i9");
     img.style.filter = "grayscale(100%)";
 }
 
-const gBtn = document.getElementsByClassName("color-btn greenish");
-gBtn.addEventListener("click", makeGreenish());
-const bBtn = document.getElementsByClassName("color-btn blueish");
-bBtn.addEventListener("click", makeBlueish());
-const bwBtn = document.getElementsByClassName("grayscale-btn");
-bwBtn.addEventListener("click", makeBw());
+function createThumbnail(imgId) {
+    const img = document.getElementById(imgId);
+    img.style.border = '1px solid black';
+    img.style.borderRadius = '50%';
+    img.style.padding = '10px';
+    img.style.width = '200px';
+}
+
+function makeReddish(imgId) {
+    var img = document.getElementById(imgId);
+    img.style.filter = "hue-rotate(30deg) saturate(2)";
+}
+
+function inv(imgId) {
+    var img = document.getElementById(imgId);
+    img.style.filter = "invert(100%)";
+}
+
+function increaseBrightness(imgId) {
+    var img = document.getElementById(imgId);
+    var brightness = 50; // Increase this value to make the image brighter
+
+    // Set the brightness value for the image
+    img.style.filter = "brightness(" + brightness + "%)";
+}
+
+
+
+
+const gBtn = document.getElementsByClassName("color-btn greenish")[0];
+gBtn.addEventListener("click", makeGreenish);
+
+const bBtn = document.getElementsByClassName("color-btn blueish")[0];
+bBtn.addEventListener("click", makeBlueish);
+const rBtn = document.getElementsByClassName("color-btn reddish")[0];
+rBtn.addEventListener("click", function() { makeReddish('i11') });
+const bwBtn = document.getElementsByClassName("grayscale-btn")[0];
+bwBtn.addEventListener("click", makeBw);
+
+const tBtn = document.getElementsByClassName("thumbnail-btn")[0];
+tBtn.addEventListener("click", function() { createThumbnail('i13') });
+const ibBtn = document.getElementsByClassName("brightness-btn")[0];
+ibBtn.addEventListener("click", function() { increaseBrightness('i10') });
+const inBtn = document.getElementsByClassName("negative-btn")[0];
+inBtn.addEventListener("click", function() { inv('i4') });
